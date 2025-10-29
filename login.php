0acbd0498d82c04cfd21b65de63bcdd52587a3ff
106212908 <106212908@student.swin.edu.au>
2025-10-29 12:17:18 +0800

    Added header

--- login.php
+++ login.php
@@ -29,6 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login Page</title>
 </head>
 <body>
+    <h2>Login Form</h2>
     <form action="login.php" method="post">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
